9f998c2b447e6ba8b13de6b3b83d73da76b80db4
danudeth650710079 <ninkuha_d@silpakorn.edu>
2024-12-21 16:00:46 +0700

    container

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title:
+                const Text('Flutter Demo', 
+                style: TextStyle(color: Colors.red)),
+            leading: const Icon(Icons.add),
+            backgroundColor: Colors.yellow,
+          ),
+          body: Container(
+            color: Colors.orange,
+            width: 100,
+            height: 100,
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.all(20),
+            child: Text("Hello"),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print("Hello");
+            },
+            child: const Icon(Icons.phone),
+          ),
+        ));
   }
 }
